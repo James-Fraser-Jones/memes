9c0d9ca9cb2af1e4cac92128867403063e4af437
smudgecat123 <sha.dow@hotmail.co.uk>
2019-04-09 00:17:10 +0100

    tidy up and refocus

--- app/Main.hs
+++ app/Main.hs
@@ -22,42 +22,63 @@ import Text.Read
 import qualified Data.Text as T
 import Test.WebDriver
 import Test.WebDriver.Class
+import Test.WebDriver.Commands.Wait
 import Text.HTML.Scalpel.Core
 import qualified Text.HTML.Scalpel.Core as S
+import Data.Time.Clock
 
 --import qualified Text.HTML.TagSoup as TagSoup
 
-data ArticleInfo = ArticleInfo {title :: T.Text, tags :: [T.Text], time :: Int, points :: Int, comments :: Int} deriving Show
+data GagPost = Post {gp_id :: T.Text, gp_time :: UTCTime, gp_title :: T.Text, gp_tags :: [T.Text]}
 
 main :: IO ()
 main = runSession (useBrowser chrome defaultConfig) processFresh
 
+headless :: WDConfig -> WDConfig
+headless config = config {wdCapabilities = headles $ wdCapabilities config}
+  where headles capabilities = capabilities {browser = headle $ browser capabilities}
+        headle browser = browser {chromeOptions = "headless" : chromeOptions browser}
+
 processFresh :: (WebDriver wd, MonadIO wd) => wd ()
 processFresh = do
-  openPage "https://www.9gag.com/hot"
-  freshSource <- getSource
-  let articleSearch = scrapeStringLike freshSource $ attrs "id" $ tagSelector "article"
-  maybe searchError processArticles articleSearch
-    where searchError = liftIO $ print "Error: No Articles Found"
-          processArticles = traverse_ processArticle . fmap (T.append "https://www.9gag.com/gag/" . T.drop 10) . filter (/= "")
+  openPage "https://www.9gag.com/fresh"
+  closePopupButton <- findElem $ ByClass "closebutton_closeButton--3abym"
+  click closePopupButton
+  -- freshSource <- getSource
+  -- let articleSearch = scrapeStringLike freshSource $ attrs "id" $ tagSelector "article"
+  -- maybe searchError processArticles articleSearch
+  --   where searchError = liftIO $ print "Error: No Articles Found"
+  --         processArticles = traverse_ processArticle . fmap (T.append "https://www.9gag.com/gag/" . T.drop 10) . filter (/= "")
 
 processArticle :: (WebDriver wd, MonadIO wd) => T.Text -> wd ()
 processArticle url = do
   openPage $ T.unpack url
   articleSource <- getSource
   let infoSearch = scrapeStringLike articleSource articleScraper
-  liftIO $ print $ maybe "Error: No Info Found" show infoSearch
+  pure ()
 
-articleScraper :: Scraper T.Text ArticleInfo
-articleScraper = ArticleInfo <$>
-                (text $ article // ("h1" @: [])) <*>
-                (pure []) <*>
-                (pure 0) <*>
-                fmap readPoints (text $ article // ("a" @: ["class" @= "point badge-evt"])) <*>
-                fmap readComments (text $ article // ("a" @: ["class" @= "comment badge-evt"]))
-  where article = ("article" @: [])
+articleScraper :: Scraper T.Text GagPost
+articleScraper = undefined
 
---------------------------------------------------------------------------------
+safeRead :: Read a => T.Text -> Maybe a
+safeRead = readMaybe . T.unpack
+
+{-------------------------------------------------------------------------------
+Algorithm:
+1. Open fresh.
+2. Scroll as far as necessary to load articles up to ones previously seen, or a maximum, whichever is smaller.
+3. Collect ids for all of these
+4. traverse article processing function
+5. Make sure to use safe read when creating Gagposts.
+6. Add to database
+7. Run every x seconds. (60?)
+8.
+-}
+
+{-------------------------------------------------------------------------------
+Code Graveyard:
+
+data ArticleInfo = ArticleInfo {title :: T.Text, tags :: [T.Text], time :: Int, points :: Int, comments :: Int} deriving Show
 
 readComments :: T.Text -> Int
 readComments text = maybe (-1) id $ safeRead firstWord
@@ -69,5 +90,12 @@ readPoints text
   | otherwise = maybe (-1) id $ safeRead firstWord
   where firstWord = T.filter (/= ',') $ head $ T.words text
 
-safeRead :: Read a => T.Text -> Maybe a
-safeRead = readMaybe . T.unpack
+articleScraper :: Scraper T.Text ArticleInfo
+articleScraper = ArticleInfo <$>
+                (text $ article // ("h1" @: [])) <*>
+                (pure []) <*>
+                (pure 0) <*>
+                fmap readPoints (text $ article // ("a" @: ["class" @= "point badge-evt"])) <*>
+                fmap readComments (text $ article // ("a" @: ["class" @= "comment badge-evt"]))
+  where article = ("article" @: [])
+-}
